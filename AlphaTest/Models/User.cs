97ba0de573f3a0974a9d195f77eab7897482d0af
iameph <iameph@yandex.ru>
2018-10-21 14:58:08 +0500

    Основные сущности

---
+++ AlphaTest/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace AlphaTest.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public virtual Query Queries { get; set; }
+    }
+}
