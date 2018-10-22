115c14cd1b059e986f10622b57c840704d0c7cc8
snd <snd@datakrat.ru>
2018-10-22 12:16:19 +0500

    Подключение Twitter Bootstrap

---
+++ AlphaTest/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Optimization;
+
+namespace AlphaTest
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/jquery-{version}.min.js",
+                "~/Scripts/bootstrap.bundle.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/site.css",
+                "~/Content/css/bootstrap.min.css"));
+        }
+    }
+}
